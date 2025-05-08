cca05a27a4e343d2f952f5efc8a74030ebe63296
Nafiul Alam Miraj <nafiulmiraj@Nafiuls-MacBook-Air-2.local>
2025-05-08 18:58:35 +0600

    top districts api completed

--- apps/topdistricts/api/v1/views.py
+++ apps/topdistricts/api/v1/views.py
@@ -13,40 +13,13 @@ class MyDistrictView(APIView):
         for district in data.get('districts', []):
             lat_long_list.append({
                 'name': district.get('name'),
-                'lat': district.get('lat'),
-                'long': district.get('long'),
-                'hourly_temperature': [],
+                'lat': "{:.2f}".format(float(district.get('lat'))),
+                'long': "{:.2f}".format(float(district.get('long'))),
             })
         top_coolest_city = GetTopDistrict.get_top_districts(lat_long_list)
                
         return Response(top_coolest_city,status=status.HTTP_201_CREATED)
     
-        
-# class TemperatureFetch(APIView, MyDistrictView):
-#       def get(self,request):
-#         temp_url = f"https://api.open-meteo.com/v1/forecast?latitude={MyDistrictView.get()}&longitude=13.41&hourly=temperature_2m"
-#         temp_response = requests.get(temp_url)
-#         temp_data = temp_response.json()
-#         hourly_data = temp_data.get("hourly", {})
-#         times = hourly_data.get("time", [])
-#         temperatures = hourly_data.get("temperature_2m", [])
-#         return Response({
-#             "times": times,
-#             "temperatures": temperatures
-#         }, status=status.HTTP_201_CREATED)
-        
-# class PmFetch(APIView):
-#     def get(self,request):
-#         pm_url = "https://air-quality-api.open-meteo.com/v1/air-quality?latitude=52.52&longitude=13.41&hourly=pm2_5"
-#         pm_response=requests.get(pm_url)
-#         pm_data=pm_response.json()
-#         hourly_data = pm_data.get("hourly",{})
-#         times = hourly_data.get("time",[])
-#         pms = hourly_data.get("pm2_5",[])
-#         return Response({
-#             "times":times,
-#             "pms":pms
-#         }, status=status.HTTP_201_CREATED)
         
         
         
--- apps/topdistricts/services.py
+++ apps/topdistricts/services.py
@@ -4,29 +4,58 @@ from apps.topdistricts.constants import TEMPERATURE_DATA
 class GetTopDistrict:
     def get_top_districts(lat_long_list):
         temp_list = []
-        avg_temp_list=[]
-        # for data in TEMPERATURE_DATA:
-    
+        pm_list = []
+        average_temp_list=[] 
+        average_pm_list=[]
         
-        # for district in lat_long_list:
-        #     # temp_url = f"https://api.open-meteo.com/v1/forecast?latitude={district.get("lat")}&longitude={district.get("long")}&hourly=temperature_2m"
-        #     # temp_response = requests.get(temp_url)
-        #     # temp_data = temp_response.json()
-        #     # temp_list.append(temp_data)
-            
-        #     print(temp_list)  
+        #api of temperature
+        for district in lat_long_list:
+            temp_url = f"https://api.open-meteo.com/v1/forecast?latitude={district.get("lat")}&longitude={district.get("long")}&hourly=temperature_2m"
+            temp_response = requests.get(temp_url)
+            temp_data = temp_response.json()
+            temp_list.append(temp_data)
+        #api of pm2_5
+        for district in lat_long_list:
+            pm_url=f"https://air-quality-api.open-meteo.com/v1/air-quality?latitude={district.get("lat")}&longitude={district.get("long")}&hourly=pm2_5"
+            pm_response= requests.get(pm_url)
+            pm_data=pm_response.json()
+            pm_list.append(pm_data)
+            print(pm_list)
         global_index_list = []
-        for avg_temp in TEMPERATURE_DATA:
+        
+        for avg_temp in temp_list:
             times = avg_temp.get("hourly").get("time")
             global_index_list = [i for i, s in enumerate(times) if 'T14' in s]
             break
-        for avg_temp in TEMPERATURE_DATA:
+        for avg_temp in temp_list:
+            lat = avg_temp.get("latitude")
+            lang = avg_temp.get("longitute")
             temperatures = avg_temp.get("hourly").get("temperature_2m")
             temperature_list = [temp for i, temp in enumerate(temperatures) if i in global_index_list]
-            print("temperature_list===============: ")
-            # print(temperature_list)
-            print(temperature_list)
+            for i in temperature_list:
+                average_temp=sum(temperature_list)/len(temperature_list)
+            average_temp_list.append({
+                'temperature':f"{average_temp:.2f}"
+            })
+        for avg_pm in pm_list:
+            lat = avg_pm.get("latitude")
+            lang = avg_pm.get("longitute")
+            pms= avg_pm.get("hourly").get("pm2_5")
+            pm_list = [pm for i, pm in enumerate(pms) if i in global_index_list]
+            for i in pm_list:
+                average_pm=sum(pm_list)/len(pm_list)
+            average_pm_list.append({
+                'pm_2.5':f"{average_pm:.2f}"
+            })       
+        for i,(d,u) in enumerate(zip(lat_long_list,average_temp_list)):
+            d.update(u)
+            lat_long_list[i]=d
+        
+        for i,(d,u) in enumerate(zip(lat_long_list,average_pm_list)):
+            d.update(u)
+            lat_long_list[i]=d
             
-                    
-        print(global_index_list)
-        return temp_list
+        final_list=sorted(lat_long_list, key=lambda x:(x['temperature'],x['pm_2.5']))
+        print(final_list)
+
+        return final_list
